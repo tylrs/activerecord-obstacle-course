89d517800cf03ba87d6c7e5eb984173e43a7d969
Taylor Galloway <galloway.taylor@gmail.com>
2022-10-31 10:17:48 -0500

    Complete week 4

--- spec/models/aroc_week_4_spec.rb
+++ spec/models/aroc_week_4_spec.rb
@@ -13,11 +13,12 @@ describe 'ActiveRecord Obstacle Course, Week 4' do
 
   it '21. calculates the total sales' do
     # ---------------------- Using Ruby -------------------------
-    total_sales = Order.all.map(&:amount).inject(:+)
+    # total_sales = Order.all.map(&:amount).inject(:+)
     # -----------------------------------------------------------
 
     # ------------------ Using ActiveRecord ---------------------
     # Solution goes here
+    total_sales = Order.all.sum("amount")
     # -----------------------------------------------------------
 
     # Expectation
@@ -26,14 +27,15 @@ describe 'ActiveRecord Obstacle Course, Week 4' do
 
   it '22. calculates the total sales for all but one user' do
     # ---------------------- Using Ruby -------------------------
-    orders = Order.all.map do |order|
-      order if order.user_id != @user_2.id
-    end.select{|i| !i.nil?}
-    total_sales = orders.map(&:amount).inject(:+)
+    # orders = Order.all.map do |order|
+    #   order if order.user_id != @user_2.id
+    # end.select{|i| !i.nil?}
+    # total_sales = orders.map(&:amount).inject(:+)
     # -----------------------------------------------------------
 
     # ------------------ Using ActiveRecord ---------------------
     # Solution goes here
+    orders = Order.where.not(id: @user_2.id ).sum("amount")
     # -----------------------------------------------------------
 
     # Expectation
@@ -44,12 +46,13 @@ describe 'ActiveRecord Obstacle Course, Week 4' do
     expected_result = [@order_3, @order_11, @order_5, @order_13, @order_10, @order_15, @order_9]
 
     # ------------------ Inefficient Solution -------------------
-    order_ids = OrderItem.where(item_id: @item_4.id).map(&:order_id)
-    orders = order_ids.map { |id| Order.find(id) }
+    # order_ids = OrderItem.where(item_id: @item_4.id).map(&:order_id)
+    # orders = order_ids.map { |id| Order.find(id) }
     # -----------------------------------------------------------
 
     # ------------------ Improved Solution ----------------------
     #  Solution goes here
+    orders = Order.where(id: OrderItem.where(item_id: @item_4.id).pluck(:order_id))
     # -----------------------------------------------------------
 
     # Expectation
@@ -60,13 +63,15 @@ describe 'ActiveRecord Obstacle Course, Week 4' do
     expected_result = [@order_11, @order_5]
 
     # ------------------ Inefficient Solution -------------------
-    orders = Order.where(user: @user_2)
-    order_ids = OrderItem.where(order_id: orders, item: @item_4).map(&:order_id)
-    orders = order_ids.map { |id| Order.find(id) }
+    # orders = Order.where(user: @user_2)
+    # order_ids = OrderItem.where(order_id: orders, item: @item_4).map(&:order_id)
+    # orders = order_ids.map { |id| Order.find(id) }
     # -----------------------------------------------------------
 
     # ------------------ Improved Solution ----------------------
     #  Solution goes here
+    #  Grab all 
+    orders = Order.where(user: @user_2, id: OrderItem.where(item_id: @item_4.id).pluck(:order_id))
     # -----------------------------------------------------------
 
     # Expectation
@@ -78,17 +83,18 @@ describe 'ActiveRecord Obstacle Course, Week 4' do
     expected_result = [@item_1, @item_4, @item_9, @item_2, @item_5, @item_10, @item_3, @item_8, @item_7]
 
     # ----------------------- Using Ruby -------------------------
-    items = Item.all
+    # items = Item.all
 
-    ordered_items = items.map do |item|
-      item if item.orders.present?
-    end
+    # ordered_items = items.map do |item|
+    #   item if item.orders.present?
+    # end
 
-    ordered_items = ordered_items.compact
+    # ordered_items = ordered_items.compact
     # ------------------------------------------------------------
 
     # ------------------ ActiveRecord Solution ----------------------
     # Solution goes here
+    ordered_items = Item.where(id: OrderItem.all.distinct.pluck(:item_id))
     # ---------------------------------------------------------------
 
     # Expectations
